effa89d0072dbcbceb2959068c5ba526e83de7f9
Christian Ashby <git@cashby.me.uk>
2020-05-17 11:07:44 +0100

    Modifying SERVO0 pin for my ender 3 on the EXT_A0 connector

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "CA Ender-3 v0.1"
+#define CUSTOM_MACHINE_NAME "CA Ender-3 v0.2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
